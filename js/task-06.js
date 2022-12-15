3c144fdc8927adea2ffc5cc348ecc0f1eeeb91e6
Vladislav013 <shevchenkovladislav10@gmail.com>
2022-12-15 20:20:13 +0200

    update-mentore

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,9 @@
 const inputEl = document.querySelector('#validation-input');
+
 inputEl.addEventListener('blur', () => {
     if (inputEl.value.length === Number(inputEl.dataset.length)) {
         inputEl.classList.add('valid');
-        inputEl.classList.remote('invalid');
+        inputEl.classList.remove('invalid');
     } else {
         inputEl.classList.add('invalid');
         inputEl.classList.remove('valid')
